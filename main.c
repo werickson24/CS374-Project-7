0c7bbb1bb07dd90df7513260bbb40047eff9521e
William Erickson <william@ericksonls.com>
2024-02-22 20:30:49 -0800

    Finished part 2

---
+++ main.c
@@ -0,0 +1,44 @@
+﻿#include "myalloc.h"
+#include <stdio.h>
+
+void example1();
+void example2();
+void example3();
+
+int main(void){
+    printf("\nExample 1: =========================\n");
+    //example1();
+    printf("\nExample 2: =========================\n");
+    //example2();
+    printf("\nExample 3: =========================\n");
+    //example3();
+}
+
+void example1(){
+    void *p;
+
+    p = myalloc(512);
+    print_data();
+
+    myfree(p);
+    print_data();
+}
+
+void example2(){
+    myalloc(10); print_data();
+    myalloc(20); print_data();
+    myalloc(30); print_data();
+    myalloc(40); print_data();
+    myalloc(50); print_data();
+}
+
+void example3(){
+    void *p;
+
+    myalloc(10);     print_data();
+    p = myalloc(20); print_data();
+    myalloc(30);     print_data();
+    myfree(p);       print_data();
+    myalloc(40);     print_data();
+    myalloc(10);     print_data();
+}
